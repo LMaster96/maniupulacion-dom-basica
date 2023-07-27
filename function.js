d96e0b7116fcd45f001360301bc452b04ee34fee
Luis Mejia <luismejia9621@gmail.com>
2023-07-26 22:19:40 -0500

    Agregar .gitignore y primeros archivos del proyecto

---
+++ function.js
@@ -0,0 +1,5 @@
+function nickName(name, lastName, nickName){
+    var completeName = name + lastName;
+    return console.log("Mi nombre es " + completeName + ", pero prefiero que me digas " + nickName + ".")
+}
+nickName("Luis ", "Mejia", "Lmaster96")
